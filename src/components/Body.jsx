9fb468eedf2a657cdaf7bd9e6a8af1dad6c30724
Salvo <salvatore.disalvo@storyful.com>
2022-10-27 13:04:05 +0100

    Pagination draft

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,47 +1,56 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Pagination from 'react-bootstrap/Pagination';
 import { NewStories } from '../features/stories/NewStories';
 
-let active = 2;
-let items = [];
-for (let number = 1; number <= 5; number++) {
-  items.push(
-    <Pagination.Item key={number} active={number === active}>
-      {number}
-    </Pagination.Item>
-  );
-}
-
 function Body() {
+  const [active, setActive] = useState(1);
+  const [navigationButtons, setNavigationButtons] = useState([]);
+
+  useEffect(() => {
+    let items = [];
+    for (let number = 1; number <= 5; number++) {
+      items.push(
+        <Pagination.Item
+          key={number}
+          active={number === active}
+          onClick={() => setActive(number)}
+        >
+          {number}
+        </Pagination.Item>
+      );
+    }
+    setNavigationButtons(items);
+  }, [active]);
   return (
     <Container className='stories-grid'>
       <Row>
         <NewStories />
       </Row>
       <Row>
-        <Col>
-          <Pagination>
-            <Pagination.First />
-            <Pagination.Prev />
-            <Pagination.Item>{1}</Pagination.Item>
-            <Pagination.Ellipsis />
+        <Pagination>
+          {/* <Pagination.First />
+          <Pagination.Prev />
+          <Pagination.Item>{1}</Pagination.Item>
+          <Pagination.Ellipsis />
 
-            <Pagination.Item>{10}</Pagination.Item>
-            <Pagination.Item>{11}</Pagination.Item>
-            <Pagination.Item active>{12}</Pagination.Item>
-            <Pagination.Item>{13}</Pagination.Item>
-            <Pagination.Item disabled>{14}</Pagination.Item>
+          <Pagination.Item>{10}</Pagination.Item>
+          <Pagination.Item>{11}</Pagination.Item>
+          <Pagination.Item active>{12}</Pagination.Item>
+          <Pagination.Item>{13}</Pagination.Item>
+          <Pagination.Item disabled>{14}</Pagination.Item>
 
-            <Pagination.Ellipsis />
-            <Pagination.Item>{20}</Pagination.Item>
-            <Pagination.Next />
-            <Pagination.Last />
-          </Pagination>
-        </Col>
+          <Pagination.Ellipsis />
+          <Pagination.Item>{20}</Pagination.Item>
+          <Pagination.Next />
+          <Pagination.Last /> */}
+
+          {active > 2 && <Pagination.First />}
+          {active > 1 && <Pagination.Prev />}
+          {navigationButtons}
+        </Pagination>
       </Row>
     </Container>
   );
--- src/features/stories/NewStories.js
+++ src/features/stories/NewStories.js
@@ -6,6 +6,10 @@ import newsPlaceholder from '../../assets/news-placeholder.png';
 export function NewStories() {
   NewStoriesService();
   const stories = useSelector((state) => state.newStories.stories);
+  const paginatedStories = paginate(stories, 12, 1);
+  function paginate(array, page_size, page_number) {
+    return array.slice((page_number - 1) * page_size, page_number * page_size);
+  }
   const getDomain = (url) => {
     const originalURL = new URL(url);
     const domain = originalURL.hostname;
@@ -14,8 +18,8 @@ export function NewStories() {
 
   return (
     <>
-      {stories &&
-        stories.map((story, key) => (
+      {paginatedStories &&
+        paginatedStories.map((story, key) => (
           <Card key={key}>
             <a target='_blank' rel='noreferrer' href={story.url}>
               <Card.Img variant='top' src={newsPlaceholder} />
